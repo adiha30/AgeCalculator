6b751bccb29b0f4077f1250323663029f4282b86
adiharel <adi.harel10@gmail.com>
2023-05-20 19:32:38 +0300

    Final

--- main.js
+++ main.js
@@ -8,20 +8,25 @@ document.addEventListener("DOMContentLoaded", function() {
     calcBtn.addEventListener("click", function(event) {
         event.preventDefault();
 
-        var nowDate = new Date();
-        var givenDate = setGivenDate();
+        if(isBetween(dayInput.value,1,31) && isBetween(monthInput.value,1,12) &&
+            isBetween(yearInput.value, 0, 9999)) {
+                var nowDate = new Date();
+                var givenDate = setGivenDate();
 
-        const diffMs = nowDate.getTime() - givenDate.getTime();
+                const diffMs = nowDate.getTime() - givenDate.getTime();
 
-        const diffDate = new Date(diffMs);
-        const years = diffDate.getUTCFullYear() - 1970;
-        const months = diffDate.getUTCMonth();
-        const days = diffDate.getUTCDate() + 1;
-        console.log(days);
+                const diffDate = new Date(diffMs);
+                const years = diffDate.getUTCFullYear() - 1970;
+                const months = diffDate.getUTCMonth();
+                const days = diffDate.getUTCDate() + 1;
+                console.log(days);
 
-        setDiffInPage(document.getElementById("years"), years);
-        setDiffInPage(document.getElementById("months"), months);
-        setDiffInPage(document.getElementById("days"), days);
+                setDiffInPage(document.getElementById("years"), years);
+                setDiffInPage(document.getElementById("months"), months);
+                setDiffInPage(document.getElementById("days"), days);
+        } else {
+            alert("Day: 1-31, Month: 1-12, Year must be between 0 and 9999");
+        }
     });
   });
 
@@ -38,3 +43,7 @@ const setGivenDate = () => {
 const setDiffInPage = (element, value) => {
     element.textContent = value;
 };
+
+const isBetween = (value,startValue, endValue) => {
+    return value >= startValue && value <= endValue;
+}
